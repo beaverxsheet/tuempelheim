94a238aa53da5dbcefb90a668186193d3d37fa68
beaverxsheet <beaverxsheet@gmail.com>
2019-05-24 13:57:47 +0200

    added player

---
+++ Scripts/Player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody
+
+const MOVE_VEL = 10
+const MOUSE_SENS = 0.5
+const GRAV = 5
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= MOUSE_SENS * event.relative.x
+		rotation_degrees.x -= MOUSE_SENS * event.relative.y
+
+func _process(delta):
+	if Input.is_action_pressed("exit"):
+		get_tree().quit()
+
+func _physics_process(delta):
+	var move_vec = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		move_vec.z -= 1
+	if Input.is_action_pressed("move_backward"):
+		move_vec.z += 1
+	if Input.is_action_pressed("move_left"):
+		move_vec.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_vec.x += 1
+	move_vec = move_vec.normalized()
+	move_vec = move_vec.rotated(Vector3(0,1,0), rotation.y) * MOVE_VEL
+	move_vec -= Vector3(0,GRAV,0)
+	move_and_collide(move_vec * delta)
+	
+	
