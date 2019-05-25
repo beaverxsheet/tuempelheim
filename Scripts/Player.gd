21cd1c07883fd4d1648b7098f9618df791796e64
beaverxsheet <beaverxsheet@gmail.com>
2019-05-25 12:43:33 +0200

    add skyboxes and fix camera movent

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody
 
 const MOVE_VEL = 10
-const MOUSE_SENS = 0.5
+const MOUSE_SENS = 0.3
 const GRAV = 5
 
 func _ready():
@@ -11,7 +11,11 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= MOUSE_SENS * event.relative.x
-		rotation_degrees.x -= MOUSE_SENS * event.relative.y
+		if not rotation_degrees.x >= 85 or not rotation_degrees.x <= -85:
+			rotation_degrees.x -= MOUSE_SENS * event.relative.y
+		if rotation_degrees.x >= 85: rotation_degrees.x = 85
+		if rotation_degrees.x <= -85: rotation_degrees.x = -85
+		print(rotation_degrees.x)
 
 func _process(delta):
 	if Input.is_action_pressed("exit"):
@@ -30,6 +34,7 @@ func _physics_process(delta):
 	move_vec = move_vec.normalized()
 	move_vec = move_vec.rotated(Vector3(0,1,0), rotation.y) * MOVE_VEL
 	move_vec -= Vector3(0,GRAV,0)
-	move_and_collide(move_vec * delta)
+	move_and_slide(move_vec)
+	#move_and_collide(move_vec*delta)
 	
 	
