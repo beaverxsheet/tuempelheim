6fef2947948d6d08107682a5a7f0cc358263314a
anton <nelsontrafalgar001@gmail.com>
2019-05-25 15:37:21 +0200

    Minor edits with the keyframing. Major issue is that the cutout anim does not fit into the viewport at all times.

--- Scripts/NPC_Vessel.gd
+++ Scripts/NPC_Vessel.gd
@@ -1,5 +1,11 @@
 extends "res://Scripts/Sprite3D_controls.gd"
 
+onready var animplayer_child = get_parent().get_node("Viewport/TestTyp/AnimationPlayer")
+
 func _process(delta):
 #	print(get_Sprite_angle())
 #	print(get_Distance(get_Player()))
+	if get_Distance(get_Player()) < 30:
+		animplayer_child.play("Dance")
+	else:
+		animplayer_child.play("Wave")
