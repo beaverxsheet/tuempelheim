fcc3737a6877d5dfc895a7eaaeb3414cf08b621f
anton <nelsontrafalgar001@gmail.com>
2019-05-25 15:19:30 +0200

    Added some scripting with inheritance, beginning on some much needed functionality

---
+++ Scripts/NPC_Vessel.gd
@@ -0,0 +1,5 @@
+extends "res://Scripts/Sprite3D_controls.gd"
+
+func _process(delta):
+#	print(get_Sprite_angle())
+#	print(get_Distance(get_Player()))
---
+++ Scripts/Sprite3D_controls.gd
@@ -0,0 +1,27 @@
+extends Sprite3D
+
+onready var camera = get_tree().get_root().get_camera()
+
+func get_Sprite_angle():
+	# Sprite Angle gives direction from which player faces NPC/Object
+	# 0 - Front
+	# 1 - Right
+	# 2 - Back
+	# 3 - Left
+
+	var dir = (global_transform.origin - camera.global_transform.origin).normalized()
+	var angle = rad2deg(atan2(dir.z, dir.x)) + get_parent().rotation_degrees.y
+	angle = wrapi(angle, 0, 360)
+
+	var sprite_angle = int(floor(angle / 90) - 2)
+	sprite_angle = wrapi(sprite_angle, 0, 4)
+
+	return sprite_angle
+	
+func get_Player():
+	# Gets Player
+	return get_tree().get_root().get_node("/root/World/Player")
+	
+func get_Distance(target):
+	# Gives distance from target
+	return (global_transform.origin - target.global_transform.origin).length()
