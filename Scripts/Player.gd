1bd4568e37e992a772c8f936c75b2dd5179279fe
beaverxsheet <beaverxsheet@gmail.com>
2019-05-26 18:00:32 +0200

    removing debug and changing keybindings

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,6 +4,8 @@ const MOVE_VEL = 10
 const MOUSE_SENS = 0.3
 const GRAV = 5
 
+var cam_on = true
+
 onready var fps = $CanvasLayer/Label
 
 func _ready():
@@ -11,16 +13,22 @@ func _ready():
 	pass
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and cam_on:
 		rotation_degrees.y -= MOUSE_SENS * event.relative.x
 		if not rotation_degrees.x >= 85 or not rotation_degrees.x <= -85:
 			rotation_degrees.x -= MOUSE_SENS * event.relative.y
 		if rotation_degrees.x >= 85: rotation_degrees.x = 85
 		if rotation_degrees.x <= -85: rotation_degrees.x = -85
-		print(rotation_degrees.x)
+		#print(rotation_degrees.x)
 
 func _process(delta):
-	if Input.is_action_pressed("exit"):
+	if Input.is_action_just_pressed("exit") and cam_on:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		cam_on = false
+	elif Input.is_action_just_pressed("exit") and not cam_on:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		cam_on = true
+	if Input.is_action_pressed("end"):
 		get_tree().quit()
 	fps.text = str(Engine.get_frames_per_second())
 
