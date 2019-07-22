82783082df838c1c2519f6c04ee39b71d6c58660
anton <nelsontrafalgar001@gmail.com>
2019-07-22 13:18:29 +0200

    Camera view is now higher to reflect realistic body height. Smooth animation changes. Animations now also run and are changed on hidden sprites.

--- Scripts/NPC_Vessel.gd
+++ Scripts/NPC_Vessel.gd
@@ -11,11 +11,9 @@ func _process(delta):
 	if get_Sprite_angle() == 0:
 		get_parent().get_node("Viewport/TestTyp").hide()
 		get_parent().get_node("Viewport/Testtyp_Back").show()
-		animplayer_child = animplayer_child_back
 	else:
 		get_parent().get_node("Viewport/TestTyp").show()
 		get_parent().get_node("Viewport/Testtyp_Back").hide()
-		animplayer_child = animplayer_child_front
 	
 
 
@@ -31,9 +29,11 @@ func _on_ChatArea_body_exited(body):
 
 func _on_WideArea_body_entered(body):
 	if body == player_node:
-		animplayer_child.play("Dance")
+		animplayer_child_back.play("Dance")
+		animplayer_child_front.play("Dance")
 
 
 func _on_WideArea_body_exited(body):
 	if body == player_node:
-		animplayer_child.play("Wave")
+		animplayer_child_back.play("Wave")
+		animplayer_child_front.play("Wave")
