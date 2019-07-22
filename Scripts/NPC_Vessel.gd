32a4e2e0ce00a3eb096172992b5e7b53de63fc64
anton <nelsontrafalgar001@gmail.com>
2019-07-22 13:06:07 +0200

    Added "dance" animation to the back sprite. Needs some more commits so I can finally merge to master.

--- Scripts/NPC_Vessel.gd
+++ Scripts/NPC_Vessel.gd
@@ -1,16 +1,21 @@
 extends "res://Scripts/Sprite3D_controls.gd"
 
 
-onready var animplayer_child = get_parent().get_node("Viewport/TestTyp/AnimationPlayer")
+onready var animplayer_child_front = get_parent().get_node("Viewport/TestTyp/AnimationPlayer")
+onready var animplayer_child_back = get_parent().get_node("Viewport/Testtyp_Back/AnimationPlayer")
 onready var player_node = get_Player()
+
+var animplayer_child = null
 
 func _process(delta):
 	if get_Sprite_angle() == 0:
 		get_parent().get_node("Viewport/TestTyp").hide()
 		get_parent().get_node("Viewport/Testtyp_Back").show()
+		animplayer_child = animplayer_child_back
 	else:
 		get_parent().get_node("Viewport/TestTyp").show()
 		get_parent().get_node("Viewport/Testtyp_Back").hide()
+		animplayer_child = animplayer_child_front
 	
 
 
