27bf73169a0a215ff20444630d1a20a3634c310d
anton <nelsontrafalgar001@gmail.com>
2019-07-23 14:28:12 +0200

    Three main new functionalities: * now has an array containing copies of all possible objects * adding an item creates a new dictionary entry with the item as key -> value is amount of this item carried * Item amounts can be checked and updated by getters and setters taking ID and as input

--- Scenes/Ingame_Control.gd
+++ Scenes/Ingame_Control.gd
@@ -3,18 +3,42 @@ extends Control
 onready var addTo_btn = $BackgroundPanel/VBoxContainer/HBoxContainer/Add_to
 onready var lessN_btn = $BackgroundPanel/VBoxContainer/HBoxContainer/Remove_from
 
-var pencil_amount = 99
+var pencil_amount
+
+# Array containing all objects
+var itemArray = []
+
+# Dictionary containing all collected items carried
+var inventoryContents = {}
 
 func _ready():
+	itemArray.append(load("res://Scripts/ITEM_ID_0_Pencil.gd").new())
+#	itemArray.append(load("res://Scripts/ITEM_ID_1_Cup.gd").new())
+	pencil_amount = check_item_amount(0)
 	$BackgroundPanel/VBoxContainer/HBoxContainer/Amount.text = String(pencil_amount)
-	
 
 func _process(delta):
+	pencil_amount = check_item_amount(0)
 	$BackgroundPanel/VBoxContainer/HBoxContainer/Amount.text = String(pencil_amount)
 
+
+func change_item_amount(change, item_id):
+	var item = itemArray[item_id]
+	if inventoryContents.get(item, false):
+		inventoryContents[item] = inventoryContents[item] + change
+	else:
+		inventoryContents[item] = 0 + change
+		
+func check_item_amount(item_id):
+	var item = itemArray[item_id]
+	return inventoryContents.get(item, 0)
+
+
 func _on_Add_to_pressed():
-	pencil_amount += 1
+	# Add a pencil
+	change_item_amount(1, 0)
 
 
 func _on_Remove_from_pressed():
-	pencil_amount -= 1
+	# Remove a pencil
+	change_item_amount(-1, 0)
---
+++ Scripts/ITEM_ID_1_Cup.gd
@@ -0,0 +1,8 @@
+# Item: Cup
+# ID 1
+
+extends Ingame_Item
+
+func _ready():
+	item_ID = 1
+	item_name = "Cup"
