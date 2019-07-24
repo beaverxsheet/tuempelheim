8085bb56f86627384753ee3366ef4e6cf352d851
anton <nelsontrafalgar001@gmail.com>
2019-07-24 14:08:12 +0200

    Now also shows amount of cups in inventory. Made code less sloppy somehow. Updates amount when picking stuff up.

--- Scenes/Ingame_Control.gd
+++ Scenes/Ingame_Control.gd
@@ -15,19 +15,27 @@ func _ready():
 	globals.add_item(0, "Pencil", 1, 1)
 	globals.add_item(1, "Cup", 1, 1)
 	
-	pencil_amount = globals.check_item_amount(0)
-	$BackgroundPanel/VBoxContainer/HBoxContainer/Amount.text = String(pencil_amount)
+	$BackgroundPanel/VBoxContainer/HBoxContainer/Amount.text = String(globals.check_item_amount(0))
+	$BackgroundPanel/VBoxContainer/HBoxContainer2/Amount.text = String(globals.check_item_amount(1))
 
 func _process(delta):
-	pencil_amount = globals.check_item_amount(0)
-	$BackgroundPanel/VBoxContainer/HBoxContainer/Amount.text = String(pencil_amount)
+	$BackgroundPanel/VBoxContainer/HBoxContainer/Amount.text = String(globals.check_item_amount(0))
+	$BackgroundPanel/VBoxContainer/HBoxContainer2/Amount.text = String(globals.check_item_amount(1))
 	if Input.is_action_just_pressed("ui_down"):
 		pass
 
-func _on_Add_to_pressed():
+func _pencil_add_to_pressed():
 	# Add a pencil
 	globals.change_item_amount(1, 0)
 
-func _on_Remove_from_pressed():
+func _pencil_remove_from_pressed():
 	# Remove a pencil
 	globals.change_item_amount(-1, 0)
+
+
+func _cup_add_to_pressed():
+	globals.change_item_amount(1, 1)
+
+
+func _cup_remove_from_pressed():
+	globals.change_item_amount(-1, 1)
