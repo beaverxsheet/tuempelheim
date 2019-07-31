379b5524bbf05caf2b5a414020a88dbe91cd0eb1
anton <nelsontrafalgar001@gmail.com>
2019-07-31 13:41:05 +0200

    Now deletes entries from inventory when none of that kind are present

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -15,28 +15,19 @@ func add_item(item_ID, item_name, weight, value, item_type=0, is_unique=false):
 	item.is_unique = is_unique
 	itemArray.append(item)
 
-func change_item_amount(change, item_id, overrideNegative=false):
+func change_item_amount(change, item_id):
 	var item = globals.itemArray[item_id]
+	var newValue
 	if globals.inventoryContents.get(item, false):
 		# Execute this if the item is already in inventory
-		var newValue = globals.inventoryContents[item] + change
-		if newValue < 0 and !overrideNegative:
-			return "Cannot go below zero"
-		elif newValue < 0 and overrideNegative:
-			globals.inventoryContents[item] = newValue
-			return "Went below zero due to override"
-		else:
-			globals.inventoryContents[item] = newValue
+		newValue = globals.inventoryContents[item] + change
 	else:
 		# Create new entry if not in inventory
-		var newValue = 0 + change
-		if newValue < 0 and !overrideNegative:
-			return "Cannot go below zero"
-		elif newValue < 0 and overrideNegative:
-			globals.inventoryContents[item] = newValue
-			return "Went below zero due to override"
-		else:
-			globals.inventoryContents[item] = newValue
+		newValue = 0 + change
+	if newValue > 0:
+		globals.inventoryContents[item] = newValue
+	else:
+		globals.inventoryContents.erase(item)
 		
 func check_item_amount(item_id):
 	var item = globals.itemArray[item_id]
