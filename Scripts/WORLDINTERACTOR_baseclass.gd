47d311f5273908b24a6d580fcf4a4dfc4d2a0527
anton <nelsontrafalgar001@gmail.com>
2019-08-13 11:40:28 +0200

    Allows for discrimination between shop and chest now

--- Scripts/WORLDINTERACTOR_baseclass.gd
+++ Scripts/WORLDINTERACTOR_baseclass.gd
@@ -41,7 +41,10 @@ func interact_onclick():
 			yield(get_node("../Control/Chest/CenterBackPanel/Button"), "pressed") # Resume operations once close button pressed
 			get_node("../Player").capture_mouse_mode(true)
 		SHOP:
-			pass
+			get_node("../Player").capture_mouse_mode(false)
+			get_node("../Control").fill_shop_and_personal_itemlists(self)
+			yield(get_node("../Control/Shop/CenterBackPanel/Button"), "pressed") # Resume operations once close button pressed
+			get_node("../Player").capture_mouse_mode(true)
 			
 func change_item_amount(change, item_id):
 	var item = globals.itemArray[item_id]
