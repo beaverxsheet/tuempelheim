b913cf85df97e1f4da8283da973f2efd343210dc
anton <nelsontrafalgar001@gmail.com>
2019-08-20 12:17:03 +0200

    Worked on the GUI elements to the chat

---
+++ Scripts/Chat/ChatControl_GUI.gd
@@ -0,0 +1,30 @@
+extends Control
+
+func _ready():
+	loop_to_fill_chat_options(["hi", "no", "place"])
+
+
+func create_chat_control_node(text, ID):
+	# Create a single clickable button that allows to select a chat option
+	var newButton
+	newButton = Button.new()
+	newButton.add_font_override("font", load("res://Misc/Fonts/FONT_STANDARD_24.tres"))
+	newButton.text = String(text)
+
+	# Add signal, connect to self upon being pressed
+	newButton.connect("pressed", self, "_on_chat_option_chosen", [ID])
+
+	return newButton
+	
+
+func loop_to_fill_chat_options(inarray):
+	# Show all chat reply options on sceen by repeatedly calling the function that creates the actual buttons
+	var container = $CPanel/Scroll/VBox
+	var i = 0 # Counter to increment ID
+	for text in inarray:
+		# Create button for each option
+		container.add_child(create_chat_control_node(text, i))
+		i += 1
+		
+func _on_chat_option_chosen(ID):
+	print(ID)
