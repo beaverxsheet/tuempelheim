603758fd467f685f77d860d641114c4a0a85992f
anton <nelsontrafalgar001@gmail.com>
2019-08-28 12:50:16 +0200

    Wrote a parser and defined goapml

--- Scripts/NPC/Dijkstra_Graph.gd
+++ Scripts/NPC/Dijkstra_Graph.gd
@@ -2,16 +2,18 @@ extends Node
 # Props to Bogotobogo.com and their python implementation
 
 var vertex = preload("res://Scripts/NPC/Dijkstra_Vertex.gd")
-var vert_dict; var num_vertices;
+var vert_dict; var num_vertices; var _name;
 
-func _init():
+func _init(givenname):
 	vert_dict = {}
 	num_vertices = 0
+	_name = givenname
 	
 func add_vertex(node : String):
 	num_vertices += 1
 	var new_vertex = vertex.new(node)
 	vert_dict[node] = new_vertex
+	add_child(new_vertex)
 	return new_vertex
 	
 func get_vertex(n : String):
@@ -24,8 +26,7 @@ func add_edge(frm : String, to : String, cost = 0):
 	if !(frm in vert_dict):
 		add_vertex(frm)
 	if !(to in vert_dict):
-		add_vertex(frm)
-		
+		add_vertex(to)
 	vert_dict[frm].add_neighbour(vert_dict[to], cost)
 	vert_dict[to].add_neighbour(vert_dict[frm], cost)
 	
--- Scripts/NPC/NPC_baseclass.gd
+++ Scripts/NPC/NPC_baseclass.gd
@@ -16,35 +16,36 @@ enum {
 }
 
 func _ready():
-	print(translation)
-	print(choose_random_nearby_target())
-	
 	# DIJKSTRA
-	var g = graph.new()
-	g.add_vertex('a')
-	g.add_vertex('b')
-	g.add_vertex('c')
-	g.add_vertex('d')
-	g.add_vertex('e')
-	g.add_vertex('f')
-
-	g.add_edge('a', 'b', 7)  
-	g.add_edge('a', 'c', 9)
-	g.add_edge('a', 'f', 14)
-	g.add_edge('b', 'c', 10)
-	g.add_edge('b', 'd', 15)
-	g.add_edge('c', 'd', 11)
-	g.add_edge('c', 'f', 2)
-	g.add_edge('d', 'e', 6)
-	g.add_edge('e', 'f', 9)
+	
+	var g = parseSheetGOAP(readSheet("res://Testers/definition.goapml"))
+	
+#	var g = graph.new()
+#	g.add_vertex('a')
+#	g.add_vertex('b')
+#	g.add_vertex('c')
+#	g.add_vertex('d')
+#	g.add_vertex('e')
+#	g.add_vertex('f')
+#
+#	g.add_edge('a', 'b', 7)  
+#	g.add_edge('a', 'c', 9)
+#	g.add_edge('a', 'f', 14)
+#	g.add_edge('b', 'c', 10)
+#	g.add_edge('b', 'd', 15)
+#	g.add_edge('c', 'd', 11)
+#	g.add_edge('c', 'f', 2)
+#	g.add_edge('d', 'e', 6)
+#	g.add_edge('e', 'f', 9)
 	
 	# Check if graph is ok
 	for v in g.vert_dict.values():
 		for w in v.get_connections():
 			print(v.id, "-", w.id)
-			
-	globals.dijkstra(g, "a")
-	print(globals.shortest(g, "e"))
+	
+	print(g._name)
+#	globals.dijkstra(g, "a")
+#	print(globals.shortest(g, "e"))
 
 func interact_onclick():
 	get_parent().capture_mouse_mode(false)
@@ -70,6 +71,47 @@ func choose_target_given_vector(target_delta):
 	elif target_delta is Vector2:
 		return translation + Vector3(target_delta.x, target_delta.y, 0)
 
+func readSheet(filename):
+	# Read and return contents of a chat file NOT PARSED
+	var file = File.new()
+	file.open(filename, File.READ)
+	var content = file.get_as_text()
+	file.close()
+	return String(content)
+
+func parseSheetGOAP(content):
+	# Parser for GOAP
+	var g
+	# Return PARSED contents
+	var byLine = Array(content.split("\n"))
+	byLine.pop_back()
+	
+	for line in byLine:
+		if line[0] == "#":
+			# Ignore all comments
+			continue
+
+		# Split and clean
+		var info = Array(line.split("|"))
+		for i in len(info):
+			info[i] = info[i].strip_edges()
+		
+		match info[0]:
+			"NEWG":
+				if !g:
+					# Only create new graph if this is the only graph instantiated
+					g = graph.new(String(info[1]))
+				else:
+					pass
+			"VERT":
+				if g:
+					g.add_vertex(String(info[1]))
+			"EDGE":
+				if g:
+					g.add_edge(String(info[1]), String(info[2]), int(info[3]))
+			_:
+				pass
+	return g
 
 # OVERRIDE so the type can be referred
 func get_class(): return "NPC"
